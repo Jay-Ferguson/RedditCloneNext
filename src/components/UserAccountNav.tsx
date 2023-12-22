74484309c68f6b3c58c07441b7486a5308d67825
Jay-Ferguson <jay_ferguson_15@hotmail.com>
2023-12-22 12:30:36 -0500

    added user nav seperator

--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -1,8 +1,15 @@
 import { FC } from "react";
 import { User } from "next-auth";
 import { DropdownMenu } from "./ui/DropDownMenu";
-import { DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
+import {
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@radix-ui/react-dropdown-menu";
 import UserAvatar from "./UserAvatar";
+import { DropdownMenuContent } from "./ui/DropDownMenu";
+import Link from "next/link";
+import { DropdownMenuItem } from "./ui/DropDownMenu";
+import { signOut } from "next-auth/react";
 
 interface UserAccountNavProps {
   user: Pick<User, "name" | "image" | "email">;
@@ -13,12 +20,46 @@ const UserAccountNav: FC<UserAccountNavProps> = ({ user }) => {
     <DropdownMenu>
       <DropdownMenuTrigger>
         <UserAvatar
+          className="h-8 w-8"
           user={{
             name: user.name || null,
             image: user.name || null,
           }}
         />
       </DropdownMenuTrigger>
+
+      <DropdownMenuContent className="bg-white" align="end">
+        <div className="flex items-center justify-start gap-2 p-2">
+          <div className="flex flex-col space-y-1 leading-none">
+            {user.name && <p className="font-medium">{user.name}</p>}
+            {user.email && (
+              <p className="w-[200px] truncate text-sm text-zinc-700">
+                {user.email}
+              </p>
+            )}
+          </div>
+        </div>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem asChild>
+          <Link href="/">Feed</Link>
+        </DropdownMenuItem>
+
+        <DropdownMenuItem asChild>
+          <Link href="/r/create">create commmunity</Link>
+        </DropdownMenuItem>
+
+        <DropdownMenuItem asChild>
+          <Link href="/settings">create commmunity</Link>
+        </DropdownMenuItem>
+                    <DropdownMenuSeparator></DropdownMenuSeparator>
+
+                    <DropdownMenuItem onSelect={(event) => {
+                         event.preventDefault()
+                         signOut({
+                              callbackUrl: `${window.location.origin}/sign-in`
+                         })
+                    }} className="curosr-pointer">SignOut</DropdownMenuItem>
+      </DropdownMenuContent>
     </DropdownMenu>
   );
 };
