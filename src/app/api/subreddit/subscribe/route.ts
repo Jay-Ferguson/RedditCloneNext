b90dfc39abe10a607636022a1e2b8ed26cfce093
Jay-Ferguson <jay_ferguson_15@hotmail.com>
2024-01-15 17:27:17 -0500

    added sub joining and leaving toggle component

---
+++ src/app/api/subreddit/subscribe/route.ts
@@ -0,0 +1,52 @@
+import { getAuthSession } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { SubredditSubscriptionValidator } from "@/lib/validators/subreddit";
+import { z } from "zod";
+
+export async function POST(req: Request) {
+  try {
+    const session = await getAuthSession();
+
+    if (!session?.user) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+
+    const body = await req.json();
+
+    const { subredditId } = SubredditSubscriptionValidator.parse(body);
+
+    const subscriptionExists = await db.subscription.findFirst({
+      where: {
+        subredditId,
+        userId: session.user.id,
+      },
+    });
+    if (subscriptionExists) {
+      return new Response("you are already subed", {
+        status: 400,
+      });
+    }
+
+    await db.subscription.create({
+      data: {
+        subredditId,
+        userId: session.user.id,
+      },
+    });
+
+    await db.subscription.create({
+      data: {
+        subredditId,
+        userId: session.user.id,
+      },
+    });
+
+    return new Response(subredditId);
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return new Response("invalid request passed", { status: 422 });
+    }
+    return new Response("could not create subreddit, please try again later", 
+    { status: 500 });
+  }
+}
--- src/app/r/[slug]/layout.tsx
+++ src/app/r/[slug]/layout.tsx
@@ -1,5 +1,9 @@
 import { getAuthSession } from "@/lib/auth";
+import { db } from "@/lib/db";
 import React from "react";
+import { notFound } from "next/navigation";
+import { format } from "date-fns";
+import SubscribeLeaveToggle from "@/components/SubscribeLeaveToggle";
 
 const Layout = async ({
   children,
@@ -9,6 +13,43 @@ const Layout = async ({
   params: { slug: string };
 }) => {
   const session = await getAuthSession();
+
+  const subreddit = await db.subreddit.findFirst({
+    where: { name: slug },
+    include: {
+      posts: {
+        include: {
+          author: true,
+          votes: true,
+        },
+      },
+    },
+  });
+
+  const subscription = !session?.user
+    ? undefined
+    : await db.subscription.findFirst({
+        where: {
+          subreddit: {
+            name: slug,
+          },
+          user: {
+            id: session.user.id,
+          },
+        },
+      });
+
+  const isSubscribed = !!subscription;
+
+  if (!subreddit) return notFound();
+
+  const memberCount = await db.subscription.count({
+    where: {
+      subreddit: {
+        name: slug,
+      },
+    },
+  });
 
   return (
     <div className="sm:container max-w-7xl mx-auto h-full pt-12">
@@ -24,8 +65,36 @@ const Layout = async ({
 
             <div className="hidden md:block overflow-hidden h-fit rounded-lg border border-gray-200 order-first md:order-last">
               <div className="px-6 py-4">
-                <p className="semibold py-3">About r/ {``}</p>
+                <p className="semibold py-3">About r/{subreddit.name}</p>
               </div>
+
+              <dl className="divide-y divide-gray-100 px-6 text-sm leading-6 bg-white">
+                <div className="flex justify-between gap-x-4 py-3">
+                  <dt className="text-gray-500">Created</dt>
+                  <dd className="text-gray-700">
+                    <time dateTime={subreddit.createdAt.toDateString()}>
+                      {format(subreddit.createdAt, "MMMM d, yyyy")}
+                    </time>
+                  </dd>
+                </div>
+
+                <div className="flex justify-between gap-x-4 py-3">
+                  <dt className="text-gray-500">Member</dt>
+                  <dd className="text-gray-700">
+                    <div className="text-gray-900">{memberCount}</div>
+                  </dd>
+                </div>
+
+                {subreddit.creatorId == session?.user.id ? (
+                  <div className="flex justify-between gap-x-4 py-3">
+                    <p className="text-gray-500">you created a community</p>
+                  </div>
+                ) : null}
+
+                {subreddit.creatorId !== session?.user.id ? (
+                    <SubscribeLeaveToggle></SubscribeLeaveToggle>
+                ): null}
+              </dl>
             </div>
           </div>
         </div>
