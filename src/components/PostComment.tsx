31bb52fc5ef478d744e0066f2b00b0aee1634fa7
Jay-Ferguson <jay_ferguson_15@hotmail.com>
2024-11-12 19:58:13 -0500

    changed post comment component using usestate

--- src/components/PostComment.tsx
+++ src/components/PostComment.tsx
@@ -43,7 +43,7 @@ const PostComment: FC<PostCommentProps> = ({
         postId,
         text,
         replyToId,
-      };
+      }
       const { data } = await axios.patch(
         `/api/subreddit/post/${postId}/comment`,
         payload
