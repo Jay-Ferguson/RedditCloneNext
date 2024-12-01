7cc67530ecafb695f58199f6c50c6414e78a812c
Jay-Ferguson <jay_ferguson_15@hotmail.com>
2024-11-30 19:38:27 -0500

    added use client to search component

--- src/components/ui/SearchBar.tsx
+++ src/components/ui/SearchBar.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+
 import { Prisma, Subreddit } from "@prisma/client";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
--- src/settings/page.tsx
+++ src/settings/page.tsx
@@ -1,3 +1,15 @@
 export const metadata = {
-     
-}
+  title: "settings",
+  description: "manage your account and settings",
+};
+
+import { FC } from "react";
+import { getAuthSession } from "@/lib/auth";
+interface pageProps {}
+
+const page = async ({}) => {
+  const session = await getAuthSession();
+  return <div>page</div>;
+};
+
+export default page;
