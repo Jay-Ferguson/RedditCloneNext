6bc6b42960160447bff2473dd966bfde1b8e8535
Jay-Ferguson <jay_ferguson_15@hotmail.com>
2024-12-02 20:29:32 -0500

    added error handling in username route handler

--- src/app/api/username/route.ts
+++ src/app/api/username/route.ts
@@ -1,4 +1,7 @@
 import { getAuthSession } from "@/lib/auth";
+import { UserNameValidator } from "@/lib/validators/username";
+import { db } from "@/lib/db";
+import {z} from 'zod'
 
 export async function PATCH(req: Request) {
   try {
@@ -7,5 +10,29 @@ export async function PATCH(req: Request) {
     if (!session?.user) {
       return new Response("Unauthorized", { status: 401 });
     }
+
+    const body = await req.json();
+    const { name } = UserNameValidator.parse(body);
+
+    const username = await db.user.findFirst({
+      where: {
+        username: name,
+      },
+    });
+
+    if (username) {
+      return new Response("Username is already used", { status: 409 });
+    }
+
+    await db.user.update({
+     where: {
+          id:session.user.id,
+     },
+     data:{
+          username:name
+     }
+    })
+
+    return new Response('OK', {status:200})
   } catch (error) {}
 }
